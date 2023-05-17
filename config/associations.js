4f4f66c3eabb086a49286d9e19c35e71888041f2
dev-rafa1707 <rafa1707@gmail.com>
2023-05-17 13:54:22 -0300

    add relations on association.js

--- config/associations.js
+++ config/associations.js
@@ -1,7 +1,23 @@
 const Pessoa = require("../models/pessoa")
 const Endereco = require("../models/endereco")
+const Pedido = require("../models/pedido")
+const Produto = require("../models/product")
+// const Cor = require("../models/cor")
+const db = require("../config/dbconnection")
+
 
 Pessoa.hasOne(Endereco,{onDelete:"CASCADE"})
 Endereco.belongsTo(Pessoa,{foreingKey:"pessoaId"})
 
-module.exports = {Pessoa, Endereco}
+Pessoa.hasMany(Pedido,{onDelete:"CASCADE"})
+Pedido.belongsTo(Pessoa,{foreingKey:"pessoaId"})
+
+Pedido.belongsToMany(Produto,{through:"pedidoProduto",onDelete:"CASCADE"})
+Produto.belongsToMany(Pedido,{through:"pedidoProduto",onDelete:"CASCADE"})
+
+// Produto.belongsToMany(Cor,{through:"corProduto", onDelete:"CASCADE"})
+// Cor.belongsToMany(Produto,{through:"corProduto", onDelete:"CASCADE"})
+
+db.sync() 
+
+module.exports = {Pessoa, Endereco, Pedido, Produto}
